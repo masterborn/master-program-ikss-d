3c39b583e47a30b048eba2efeeaa695abc9e07bb
BartekFo <72879738+BartekFo@users.noreply.github.com>
2021-08-09 18:56:47 +0200

    fixing index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,3 @@
 import Example from '@components/Example';
-import Button from '@components/Button';
 
-const homeComponent = () => <Button buttonLabel="Text" />;
-
-export default homeComponent;
+export default Example;
