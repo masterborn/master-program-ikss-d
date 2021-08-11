485429ac7333b2405dffdb02e737ef0c3a286331
MilyB33 <milosz.bolewski.33@gmail.com>
2021-08-11 16:06:44 +0200

    Fixed form elements, removed unnecessary files

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -8,8 +8,6 @@ import Head from 'next/head';
 
 import GlobalStyles from '@styles/GlobalStyles';
 import theme from '@styles/theme';
-import StyledInput from '@root/components/Input/StyledInput';
-import StyledCheckbox from '@root/components/Checkbox/StyledCheckbox';
 
 const App = (props) => {
   const queryClientRef = useRef;
@@ -39,8 +37,6 @@ const App = (props) => {
           <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
         <GlobalStyles />
-        <StyledCheckbox />
-        <StyledInput icon />
       </ThemeProvider>
     </>
   );
