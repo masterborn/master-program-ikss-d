725110491a63ef0880b636d73a93c6eb9f82b2c8
KowalewskiPawel <pawelkowalewsk@gmail.com>
2021-08-12 12:27:14 +0300

    corrections after the first CR

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,22 +3,21 @@ import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
 
-import { getColor } from '@root/styles/utils';
+import { getColor, getFontWeight } from '@root/styles/utils';
 import Logo from '@root/components/Logos/Logo';
 import Button from '@root/components/Button/index';
 import Icon from '@root/components/Icon/index';
-import FBIcon from '@root/assets/icons/facebook-circle-icon.svg';
-import ISIcon from '@root/assets/icons/instagram-circle-icon.svg';
-import YTIcon from '@root/assets/icons/youTube-circle-icon.svg';
-import LNIcon from '@root/assets/icons/linkedIN-circle-icon.svg';
+import FBIcon from '@assets/icons/facebook-circle-icon.svg';
+import ISIcon from '@assets/icons/instagram-circle-icon.svg';
+import YTIcon from '@assets/icons/youTube-circle-icon.svg';
+import LNIcon from '@assets/icons/linkedIN-circle-icon.svg';
 
 const Nav = styled.div`
-  padding: 1rem 5rem;
+  padding: 1.25rem 7.5rem;
   display: flex;
   z-index: 9999;
   justify-content: space-between;
   align-items: center;
-  flex-wrap: wrap;
   background: ${getColor('white')};
   box-shadow: 0px 4px 16px rgba(97, 121, 139, 0.1);
   position: sticky;
@@ -26,7 +25,7 @@ const Nav = styled.div`
   left: 0;
   right: 0;
   @media (max-width: 768px) {
-    padding: 1rem 1rem;
+    padding: 1rem 1.25rem;
   }
 `;
 
@@ -34,7 +33,6 @@ const Menu = styled.div`
   display: flex;
   flex-direction: row;
   align-items: flex-start;
-  padding: 0px;
 
   @media (max-width: 768px) {
     display: none;
@@ -48,7 +46,7 @@ const MenuLink = styled.a`
   text-decoration: none;
   color: ${getColor('steel')};
   transition: all 0.3s ease-in;
-  font-size: 0.7rem;
+  font-weight: ${getFontWeight('buttonWeight')};
 
   &:hover {
     color: ${getColor('navy')};
@@ -66,7 +64,7 @@ const Hamburger = styled.div`
   cursor: pointer;
 
   span {
-    height: 2px;
+    height: 3px;
     width: 24px;
     background: ${getColor('ikksBlue')};
     margin-bottom: 4px;
@@ -79,18 +77,21 @@ const Hamburger = styled.div`
   }
 `;
 
-const SocialMedias = styled.div`
+const SMWrapper = styled.div`
   width: 12em;
-  display: flex;
+`;
+
+const SocialMedias = styled.div`
+  width: 13em;
+  display: none;
   justify-content: space-evenly;
   align-items: center;
   flex-wrap: wrap;
-  visibility: hidden;
 
   ${(props) =>
     props.show &&
     css`
-      visibility: visible;
+      display: flex;
     `}
 
   @media (max-width: 768px) {
@@ -152,20 +153,22 @@ function Navbar({ fblink, inlink, ytlink, lnlink, page }) {
           <MenuLink active={page === 'cooperation'}>Współpraca</MenuLink>
         </Link>
       </Menu>
-      <SocialMedias show={scrollPosition > 0.25}>
-        <a href={fblink} target="_blank" rel="noreferrer">
-          <Icon icon={FBIcon} />
-        </a>
-        <a href={inlink} target="_blank" rel="noreferrer">
-          <Icon icon={ISIcon} />
-        </a>
-        <a href={ytlink} target="_blank" rel="noreferrer">
-          <Icon icon={YTIcon} />
-        </a>
-        <a href={lnlink} target="_blank" rel="noreferrer">
-          <Icon icon={LNIcon} />
-        </a>
-      </SocialMedias>
+      <SMWrapper>
+        <SocialMedias show={scrollPosition > 0.25}>
+          <a href={fblink} target="_blank" rel="noreferrer">
+            <Icon icon={FBIcon} />
+          </a>
+          <a href={inlink} target="_blank" rel="noreferrer">
+            <Icon icon={ISIcon} />
+          </a>
+          <a href={ytlink} target="_blank" rel="noreferrer">
+            <Icon icon={YTIcon} />
+          </a>
+          <a href={lnlink} target="_blank" rel="noreferrer">
+            <Icon icon={LNIcon} />
+          </a>
+        </SocialMedias>
+      </SMWrapper>
       <Hamburger>
         <span />
         <span />
