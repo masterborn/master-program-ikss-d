82826c49d6423b6cbe312bb307be11f9840dcdd1
KowalewskiPawel <pawelkowalewsk@gmail.com>
2021-08-13 16:23:48 +0300

    Mobile Menu connected to the Navbar

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,29 @@
 import Hero from '@components/Hero/Hero';
 import HeroImagePng from '@root/assets/heroImage2.jpg';
+import Navbar from '@root/components/Navbar/Navbar';
 
 const homePage = () => (
-  <Hero
-    headerText="Przykładowy nagłówek IKSS"
-    smallText="Urna, mi condimentum amet, consectetur mauris tincidunt gravida aenean. Dignissim in sit arcu nam. Ultrices integer odio feugiat vulputate."
-    imageSrc={HeroImagePng}
-    imageAlt="Przykładowy nagłówek IKSS"
-    facebookLink="https://pl-pl.facebook.com"
-    instagramLink="https://www.instagram.com"
-    youTubeLink="https://www.youtube.com"
-    linkedInLink="https://pl.linkedin.com"
-  />
+  <>
+    <Navbar
+      urls={{
+        fblink: 'https://pl-pl.facebook.com',
+        inlink: 'https://www.instagram.com',
+        ytlink: 'https://www.youtube.com',
+        lnlink: 'https://pl.linkedin.com',
+      }}
+      page="home"
+    />
+    <Hero
+      headerText="Przykładowy nagłówek IKSS"
+      smallText="Urna, mi condimentum amet, consectetur mauris tincidunt gravida aenean. Dignissim in sit arcu nam. Ultrices integer odio feugiat vulputate."
+      imageSrc={HeroImagePng}
+      imageAlt="Przykładowy nagłówek IKSS"
+      facebookLink="https://pl-pl.facebook.com"
+      instagramLink="https://www.instagram.com"
+      youTubeLink="https://www.youtube.com"
+      linkedInLink="https://pl.linkedin.com"
+    />
+  </>
 );
 
 export default homePage;
