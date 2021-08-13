5eef189f9d06dfe6a1292d646292957c61353fbd
BartekFo <72879738+BartekFo@users.noreply.github.com>
2021-08-13 14:45:52 +0200

    Fixed bug with SM bar and added padding for better layout align

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,16 +2,28 @@ import Hero from '@components/Hero/Hero';
 import HeroImagePng from '@root/assets/heroImage2.jpg';
 
 const homePage = () => (
-  <Hero
-    headerText="Przykładowy nagłówek IKSS"
-    smallText="Urna, mi condimentum amet, consectetur mauris tincidunt gravida aenean. Dignissim in sit arcu nam. Ultrices integer odio feugiat vulputate."
-    imageSrc={HeroImagePng}
-    imageAlt="Przykładowy nagłówek IKSS"
-    facebookLink="https://pl-pl.facebook.com"
-    instagramLink="https://www.instagram.com"
-    youTubeLink="https://www.youtube.com"
-    linkedInLink="https://pl.linkedin.com"
-  />
+  <>
+    <Hero
+      headerText="Przykładowy nagłówek IKSS"
+      smallText="Urna, mi condimentum amet, consectetur mauris tincidunt gravida aenean. Dignissim in sit arcu nam. Ultrices integer odio feugiat vulputate."
+      imageSrc={HeroImagePng}
+      imageAlt="Przykładowy nagłówek IKSS"
+      facebookLink="https://pl-pl.facebook.com"
+      instagramLink="https://www.instagram.com"
+      youTubeLink="https://www.youtube.com"
+      linkedInLink="https://pl.linkedin.com"
+    />
+    <Hero
+      headerText="Przykładowy nagłówek IKSS"
+      smallText="Urna, mi condimentum amet, consectetur mauris tincidunt gravida aenean. Dignissim in sit arcu nam. Ultrices integer odio feugiat vulputate."
+      imageSrc={HeroImagePng}
+      imageAlt="Przykładowy nagłówek IKSS"
+      facebookLink="https://pl-pl.facebook.com"
+      instagramLink="https://www.instagram.com"
+      youTubeLink="https://www.youtube.com"
+      linkedInLink="https://pl.linkedin.com"
+    />
+  </>
 );
 
 export default homePage;
