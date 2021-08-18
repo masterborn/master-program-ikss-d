78ec514e659cc0dbee50d01826065e02f08d2714
Bartosz Fórmanowski <bartoszformanowski@gmail.com>
2021-08-18 16:13:00 +0200

    Removing more console logs

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,71 +8,52 @@ import Projects from '@components/Projects/Projects';
 import Values from '@components/Values/Values';
 import HttpClient from '@api/httpClient/HttpClient';
 
-const homePage = ({
-  heroData,
-  valuesHeaderData,
-  valuesFirstCardData,
-  valuesSecondCardData,
-  valuesThirdCardData,
-  projectsHeaderData,
-  cooperationHeaderData,
-}) => {
-  console.log(
-    heroData,
-    valuesHeaderData,
-    valuesFirstCardData,
-    valuesSecondCardData,
-    valuesThirdCardData,
-    projectsHeaderData,
-    cooperationHeaderData,
-  );
-  return (
-    <>
-      <Navbar
-        urls={{
-          fblink: 'https://pl-pl.facebook.com',
-          inlink: 'https://www.instagram.com',
-          ytlink: 'https://www.youtube.com',
-          lnlink: 'https://pl.linkedin.com',
-        }}
-      />
+const homePage = () => (
+  <>
+    <Navbar
+      urls={{
+        fblink: 'https://pl-pl.facebook.com',
+        inlink: 'https://www.instagram.com',
+        ytlink: 'https://www.youtube.com',
+        lnlink: 'https://pl.linkedin.com',
+      }}
+    />
 
-      <Hero
-        headerText="Przykładowy nagłówek IKSS"
-        smallText="Urna, mi condimentum amet, consectetur mauris tincidunt gravida aenean. Dignissim in sit arcu nam. Ultrices integer odio feugiat vulputate."
-        imageSrc={HeroImagePng}
-        imageAlt="Przykładowy nagłówek IKSS"
-        facebookLink="https://pl-pl.facebook.com"
-        instagramLink="https://www.instagram.com"
-        youTubeLink="https://www.youtube.com"
-        linkedInLink="https://pl.linkedin.com"
-      />
-      <Values
-        valuesHeader="Wyróżniki, wartości, X-factory organizacji"
-        valuesText="Nie koniecznie musimy tu dawać tekst, ale jak jest potrzeba i przestrzeń można rozwinąć
+    <Hero
+      headerText="Przykładowy nagłówek IKSS"
+      smallText="Urna, mi condimentum amet, consectetur mauris tincidunt gravida aenean. Dignissim in sit arcu nam. Ultrices integer odio feugiat vulputate."
+      imageSrc={HeroImagePng}
+      imageAlt="Przykładowy nagłówek IKSS"
+      facebookLink="https://pl-pl.facebook.com"
+      instagramLink="https://www.instagram.com"
+      youTubeLink="https://www.youtube.com"
+      linkedInLink="https://pl.linkedin.com"
+    />
+    <Values
+      valuesHeader="Wyróżniki, wartości, X-factory organizacji"
+      valuesText="Nie koniecznie musimy tu dawać tekst, ale jak jest potrzeba i przestrzeń można rozwinąć
       nagłówek."
-        data={[
-          {
-            icon: ValuesIcon1,
-            title: 'Największa organizacja kulturalno-sportowa',
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Platea tellus nullam vulputate sem suspendisse pellentesque ullamcorper.',
-          },
-          {
-            icon: ValuesIcon2,
-            title: 'O różnorodności projektów, że każdy znajdzie coś dla siebie',
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Platea tellus nullam vulputate sem suspendisse pellentesque ullamcorper.',
-          },
-          {
-            icon: ValuesIcon3,
-            title: 'Coś o tym, że łączycie rozwój z zabawą i poznawaniem nowych ludzi',
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Platea tellus nullam vulputate sem suspendisse pellentesque ullamcorper.',
-          },
-        ]}
-      />
-      <Projects />
-    </>
-  );
-};
+      data={[
+        {
+          icon: ValuesIcon1,
+          title: 'Największa organizacja kulturalno-sportowa',
+          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Platea tellus nullam vulputate sem suspendisse pellentesque ullamcorper.',
+        },
+        {
+          icon: ValuesIcon2,
+          title: 'O różnorodności projektów, że każdy znajdzie coś dla siebie',
+          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Platea tellus nullam vulputate sem suspendisse pellentesque ullamcorper.',
+        },
+        {
+          icon: ValuesIcon3,
+          title: 'Coś o tym, że łączycie rozwój z zabawą i poznawaniem nowych ludzi',
+          text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Platea tellus nullam vulputate sem suspendisse pellentesque ullamcorper.',
+        },
+      ]}
+    />
+    <Projects />
+  </>
+);
 
 export const getStaticProps = async () => {
   const basicContentClient = new HttpClient(
