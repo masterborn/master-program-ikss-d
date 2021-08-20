ba0b732ebe90bcb570137e2511f4158d06661d7b
Bartosz Fórmanowski <bartoszformanowski@gmail.com>
2021-08-20 17:28:22 +0200

    Flattening image and data

--- src/api/httpClient/HttpClient.js
+++ src/api/httpClient/HttpClient.js
@@ -18,16 +18,34 @@ class HttpClient {
   }
 
   getFilteredData(filterCriteria) {
-    return this.getData()
-      .then((res) => res.items.filter((item) => item.fields.identifier === filterCriteria))
+    return this.getConnectedData()
       .then((res) => {
-        const resObject = res[0].fields;
-        if (resObject.text1) {
-          const text = documentToHtmlString(resObject.text1);
-          delete resObject.text1;
-          return { ...resObject, text };
+        const filteredArrayWithSingleData = res.filter(
+          (item) => item.fields.identifier === filterCriteria,
+        );
+
+        if (filteredArrayWithSingleData[0].image1) {
+          const imageTitle = filteredArrayWithSingleData[0].image1.fields.title;
+          const imageURL = filteredArrayWithSingleData[0].image1.fields.file.url;
+
+          delete filteredArrayWithSingleData[0].fields.image1;
+
+          return {
+            imageURL,
+            imageTitle,
+            ...filteredArrayWithSingleData[0].fields,
+          };
         }
-        return resObject;
+
+        return { ...filteredArrayWithSingleData[0].fields };
+      })
+      .then((res) => {
+        if (res.text1) {
+          const text = documentToHtmlString(res.text1);
+          delete res.text1;
+          return { ...res, text };
+        }
+        return res;
       });
   }
 
@@ -35,7 +53,7 @@ class HttpClient {
     return this.getData().then((res) => {
       const assets = res.includes.Asset;
 
-      const connected = res.items.map((item) =>
+      return res.items.map((item) =>
         item.fields.image1
           ? {
               ...item,
@@ -43,8 +61,6 @@ class HttpClient {
             }
           : item,
       );
-
-      return connected;
     });
   }
 }
