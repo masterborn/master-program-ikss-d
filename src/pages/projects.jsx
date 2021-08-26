f85210a26132a4c6ee442ca418dd9370acf2648e
Bartosz Fórmanowski <bartoszformanowski@gmail.com>
2021-08-26 15:50:49 +0200

    Finished Hero for subpages

---
+++ src/pages/projects.jsx
@@ -0,0 +1,31 @@
+import Navbar from '@components/Navbar/Navbar';
+import SubpagesHero from '@components/SubpagesHero/SubpagesHero';
+import ContentfulClient from '@api/clients/ContentfulAPI';
+
+const projectsPage = ({ projectHero }) => (
+  <>
+    <Navbar
+      urls={{
+        fblink: 'https://pl-pl.facebook.com',
+        inlink: 'https://www.instagram.com',
+        ytlink: 'https://www.youtube.com',
+        lnlink: 'https://pl.linkedin.com',
+      }}
+    />
+    <SubpagesHero data={projectHero} />
+  </>
+);
+
+export const getStaticProps = async () => {
+  const basicContentClient = new ContentfulClient();
+
+  const projectHero = await basicContentClient.getFilteredData('projects-top-section');
+
+  return {
+    props: {
+      projectHero,
+    },
+  };
+};
+
+export default projectsPage;
