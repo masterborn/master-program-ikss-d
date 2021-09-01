e23402f016133999a9fc4ba19b6832df2fb4aa5e
MilyB33 <milosz.bolewski.33@gmail.com>
2021-09-01 09:17:05 +0200

    Added gradient background

--- src/components/CTASection/CTASection.jsx
+++ src/components/CTASection/CTASection.jsx
@@ -14,6 +14,7 @@ const CTASection = styled.section`
   gap: 32px;
   padding: 0 var(--pdg-center) var(--pdg-bottom);
   text-align: center;
+  background: linear-gradient(0deg, rgb(244 250 255) 0%, rgb(255 255 255 / 0%) 100%);
 
   @media (max-width: 1600px) {
     --pdg-center: 380px;
