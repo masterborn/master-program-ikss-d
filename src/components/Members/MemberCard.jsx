08d4f2ceae024e9ad0b9a626b2e62244dc6f9336
MilyB33 <milosz.bolewski.33@gmail.com>
2021-09-10 15:18:12 +0200

    fix: responsive fixes

--- src/components/Members/MemberCard.jsx
+++ src/components/Members/MemberCard.jsx
@@ -11,37 +11,38 @@ import ChevronIcon from '@assets/icons/chevron-icon.svg';
 import IconSM from '@components/Icon/IconSM';
 
 const Wrapper = styled.div`
+  --imageWidth: ${({ cardExpanded }) => (cardExpanded ? '164px' : 'clamp(80px, 10vw, 164px)')};
+
   position: relative;
   display: flex;
   flex-direction: column;
-  text-align: center;
   width: 380px;
   padding: 32px 24px 40px;
+  border-radius: 16px;
+  text-align: center;
 
   box-shadow: 3.38443px 55.8976px 80px rgba(97, 121, 139, 0.07),
     1.71337px 28.2982px 34.875px rgba(97, 121, 139, 0.04725),
     0.676885px 11.1795px 13px rgba(97, 121, 139, 0.035),
     0.148069px 2.44552px 4.625px rgba(97, 121, 139, 0.02275);
-  border-radius: 16px;
 
   @media (max-width: ${getMedias('tablet')}) {
-    width: 70%;
+    width: 65%;
     padding: 20px;
   }
 
   @media (max-width: ${getMedias('mobile')}) {
     width: 100%;
     margin: 0 24px;
+    padding: 15px;
   }
 `;
 
 const ImageWrapper = styled.div`
-  --imageWidth: clamp(80px, 10vw, 164px);
-
   position: relative;
   width: var(--imageWidth);
   padding-top: var(--imageWidth);
-  margin-bottom: 24px;
+  margin-bottom: ${({ cardExpanded }) => (cardExpanded ? '16px' : '24px')};
   border-radius: 50%;
   overflow: hidden;
 `;
@@ -113,7 +114,7 @@ const Header = styled.header`
 
   @media (max-width: ${getMedias('tablet')}) {
     flex-direction: initial;
-    gap: 24px;
+    gap: 0 24px;
 
     ${({ cardExpanded }) =>
       cardExpanded &&
@@ -135,14 +136,19 @@ const Header = styled.header`
       line-height: 17.57px;
     }
   }
+
+  @media (max-width: ${getMedias('mobile')}) {
+    gap: 0 12px;
+  }
 `;
 
 const ExpandButton = styled.button`
   display: none;
   position: absolute;
-  right: 5%;
-  top: 50%;
-  transform: translate(-50%, -50%);
+  right: 1%;
+  top: ${({ cardExpanded }) => (cardExpanded ? '15%' : '50%')};
+  transform: ${({ cardExpanded }) =>
+    cardExpanded ? 'translate(-50%, -50%) rotate(180deg)' : 'translate(-50%, -50%)'};
 
   @media (max-width: ${getMedias('tablet')}) {
     display: initial;
@@ -156,6 +162,12 @@ const MemberCard = ({ member }) => {
 
   const imageVisibility = imgSrc ? <Image src={imgSrc} layout="fill" /> : <ImagePlaceholder />;
 
+  const buttonSM = linkedinUrl && (
+    <StyledButton withIcon secondary href={linkedinUrl}>
+      LinkedIn
+    </StyledButton>
+  );
+
   return (
     <Wrapper>
       <ExpandButton cardExpanded={cardExpanded} onClick={() => setCardExpanded(!cardExpanded)}>
@@ -163,7 +175,7 @@ const MemberCard = ({ member }) => {
       </ExpandButton>
 
       <Header cardExpanded={cardExpanded}>
-        <ImageWrapper>{imageVisibility}</ImageWrapper>
+        <ImageWrapper cardExpanded={cardExpanded}>{imageVisibility}</ImageWrapper>
 
         <span>
           <h4>{name}</h4>
@@ -173,7 +185,7 @@ const MemberCard = ({ member }) => {
 
       <InfoWrapper cardExpanded={cardExpanded}>
         <p>
-          <a href={`mailto:${phone}`}>
+          <a href={`tel:${phone}`}>
             <IconSM icon={PhoneIcon} size="16px" />
             {phone}
           </a>
@@ -186,11 +198,7 @@ const MemberCard = ({ member }) => {
           </a>
         </p>
 
-        {linkedinUrl && (
-          <StyledButton withIcon secondary href={linkedinUrl}>
-            LinkedIn
-          </StyledButton>
-        )}
+        {buttonSM}
       </InfoWrapper>
     </Wrapper>
   );
