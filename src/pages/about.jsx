1d42382b85b3b8f2628b482f858d34793c19378e
KowalewskiPawel <pawelkowalewsk@gmail.com>
2021-09-14 08:58:00 +0200

    fix: DRY

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,48 +1,40 @@
 import SubPagesLayout from '@components/Layouts/SubPagesLayout';
 import ContentfulClient from '@api/clients/contentfulApi';
-import {
-  filterBasicContentData,
-  filterSocials,
-} from '@root/contentfulDataTransformers/filterData';
-import Mission from '@components/Mission/Mission';
-import History from '@components/History/History';
+import { filterBasicContentData, filterSocials } from '@root/contentfulDataTransformers/filterData';
+import MissionAndHistory from '@components/MissionAndHistory/MissionAndHistory';
 
 const aboutPage = ({ mission, history }) => (
-    <>
-    <Mission data={mission} />
-    <History data={history} />
-    </>
+  <>
+    <MissionAndHistory missionData={mission} historyData={history} />
+  </>
 );
 
-
 export const getStaticProps = async () => {
+  const basicContent = await ContentfulClient.getBasicContentData('about-us');
 
-    const basicContent = await ContentfulClient.getBasicContentData('about-us');
+  const SubPageHero = filterBasicContentData(basicContent, 'about-us-top-section');
 
-    const SubPageHero = filterBasicContentData(basicContent, 'about-us-top-section');
-    
-    const mission = filterBasicContentData(basicContent, 'about-us-content-1');
-    const history = filterBasicContentData(basicContent, 'about-us-content-2');
+  const mission = filterBasicContentData(basicContent, 'about-us-content-1');
+  const history = filterBasicContentData(basicContent, 'about-us-content-2');
 
-    const socials = await ContentfulClient.getBasicContentData('common');
-    const socialUrls = filterSocials(socials);
+  const socials = await ContentfulClient.getBasicContentData('common');
+  const socialUrls = filterSocials(socials);
 
-    const CTASection = filterBasicContentData(basicContent, 'about-us-bottom-cta');
-    
-    return {
-        props: {
-            SubPageHero,
-            mission,
-            history,
-            socialUrls,
-            CTASection,
-        }
-    };
+  const CTASection = filterBasicContentData(basicContent, 'about-us-bottom-cta');
 
+  return {
+    props: {
+      SubPageHero,
+      mission,
+      history,
+      socialUrls,
+      CTASection,
+    },
+  };
 };
 
 aboutPage.getLayout = function getLayout(page, props) {
-    return <SubPagesLayout pageProps={props}>{page}</SubPagesLayout>;
+  return <SubPagesLayout pageProps={props}>{page}</SubPagesLayout>;
 };
 
 export default aboutPage;
