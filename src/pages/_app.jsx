053bbc51c30896fa79b1768771495d49d1da6707
Bartosz Fórmanowski <bartoszformanowski@gmail.com>
2021-09-29 13:06:13 +0200

    hofix: Changed favicon

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -25,7 +25,7 @@ const App = (props) => {
       <Head>
         <title>App Name</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-        <link rel="shortcut icon" href="/favicon.png" />
+        <link rel="shortcut icon" href="/favicon.svg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
